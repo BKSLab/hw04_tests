5c6f4e429ea806ae719e01df0fc26a0dc55ed6fd
BKSLab <bks2408@mail.ru>
2023-02-28 17:38:21 +0400

    Project with edits version 13 before review

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -5,5 +5,5 @@ from typing import Any
 def year(requests: Any) -> dict[str, int]:
     """Добавляет переменную с текущим годом."""
     return {
-        'year': dt.datetime.now().year,
+        'year': int(dt.datetime.now().year),
     }
